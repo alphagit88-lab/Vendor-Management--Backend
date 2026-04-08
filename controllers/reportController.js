720038ace339eda141836a578d00e03cbc42eded
alphagit88-lab <alphagit88@gmail.com>
2026-04-08 12:04:02 +0530

    fix

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,6 +1,6 @@
 const Report = require('../models/Report');
 
-exports.getSalesSummary = async (req, res) => {
+exports.getSalesSummary = async (_req, res) => {
   try {
     const summary = await Report.getSalesSummary();
     res.json({ success: true, data: summary });
@@ -10,7 +10,7 @@ exports.getSalesSummary = async (req, res) => {
   }
 };
 
-exports.getTopCustomers = async (req, res) => {
+exports.getTopCustomers = async (_req, res) => {
   try {
     const top = await Report.getTopCustomers();
     res.json({ success: true, data: top });
@@ -20,7 +20,7 @@ exports.getTopCustomers = async (req, res) => {
   }
 };
 
-exports.getInventoryAlerts = async (req, res) => {
+exports.getInventoryAlerts = async (_req, res) => {
   try {
     const alerts = await Report.getInventoryStatus();
     res.json({ success: true, data: alerts });
